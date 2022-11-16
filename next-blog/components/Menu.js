1445b43e92f6d50364e24ce713ca7028fab71e1c
Michael Helgesen <michaelhelgesen02@gmail.com>
2022-11-16 15:34:35 +0100

    Created a dynamic menu

--- next-blog/components/Menu.js
+++ next-blog/components/Menu.js
@@ -1,15 +1,11 @@
-import { sanityClient } from "../sanity"
+const Menu = ({ test }) => {
+  return (
+    <ul style={{ listStyleType: "none", display: "flex", justifyContent: "space-between", flexWrap:"wrap", padding: "0"}}>
+      {test.map((item, index) => {
+        return <li key={index} style={{padding:"0 5px 0 0"}}>{item.title}</li>;
+      })}
+    </ul>
+  );
+};
 
-
-const Menu = ({posts}) => {
-    return (
-      <div>
-        Menu
-      </div>
-    )
-  }
-
-
-
-  export default Menu
-
+export default Menu;
