9e0807ca1b85df60fcaee33037c55538d3c6875f
Michael Helgesen <michaelhelgesen02@gmail.com>
2022-11-18 15:19:48 +0100

    created bloglist

---
+++ gatsby-blog/src/components/blogList.js
@@ -0,0 +1,58 @@
+import * as React from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
+
+const createSlug = string =>
+    string.toLowerCase().replace(/\s+/g, "-").slice(0, 200)
+
+const BlogList = () => {
+  const data = useStaticQuery(graphql`
+    query blogQuery {
+      allSanityPost(sort: {
+        fields: date,
+        order: DESC
+      }){
+        edges {
+          node {
+            _rawContent
+            date(formatString: "DD.MM.YYYY")
+            title
+            description
+            slug{
+                current
+            }
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <div>
+      {data.allSanityPost.edges.map((post, index) => (
+        <Link to={post.node.slug ? (`${post.node.slug.current}`) : (`${createSlug(post.node.title)}`)} key={index} style={{textDecoration:"none"}}>
+        <div
+          key={index}
+          style={{ 
+            padding: "20px",
+            margin: "20px 0",
+            border:"1px solid black" 
+        }}
+        >
+          <h4
+            style={{
+              margin: "0",
+              textDecoration: "none"
+            }}
+          >
+            {post.node.title}
+          </h4>
+          <p style={{margin:"0", color:"black"}}>
+            {post.node.description}
+          </p>
+        </div>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default BlogList
