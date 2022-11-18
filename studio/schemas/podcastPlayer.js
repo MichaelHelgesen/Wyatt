e0bba6b73c5f38f0b1d9745c9b1aa328b4cf7f35
Michael Helgesen <michaelhelgesen02@gmail.com>
2022-11-18 15:24:57 +0100

    update

--- studio/schemas/podcastPlayer.js
+++ studio/schemas/podcastPlayer.js
@@ -1,11 +1,3 @@
-import { React } from 'react'
-
-const Preview = (value) => {
-    //console.log(value)
-	//const { url } = value
-	return(<audio src={value} controls />)
-}
-
 export default {
     name: 'podcastPlayer',
     type: 'object',
@@ -14,13 +6,8 @@ export default {
         {
             type: "cloudinary.asset",
             name: "podcast",
+            title: "tekst",
             description: "This asset is served from Cloudinary",
         },
-    ],
-    preview: {
-        select: {
-            url: 'https://res.cloudinary.com/mikkesblogg/video/upload/v1668506637/Wyatt/Wav_868kb_r3khir.wav'
-        },
-        component: Preview
-    }
+    ],   
 }
