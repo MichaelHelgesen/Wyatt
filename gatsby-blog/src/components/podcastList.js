c19a46e12d73d2ef9b597766b94f3b2a21933643
Michael Helgesen <michaelhelgesen02@gmail.com>
2022-11-18 15:22:06 +0100

    created podcast list

---
+++ gatsby-blog/src/components/podcastList.js
@@ -0,0 +1,50 @@
+import * as React from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
+
+const createSlug = string =>
+    string.toLowerCase().replace(/\s+/g, "-").slice(0, 200)
+
+const PodcastList = () => {
+  const data = useStaticQuery(graphql`
+    query podcastQuery {
+      allSanityPodcast{
+        edges {
+          node {
+            _rawContent
+            title
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <div>
+      {data.allSanityPodcast.edges.map((post, index) => (
+        <Link to={post.node.slug ? (`${post.node.slug.current}`) : (`${createSlug(post.node.title)}`)} key={index} style={{textDecoration:"none"}}>
+        <div
+          key={index}
+          style={{ 
+            padding: "20px",
+            margin: "20px 0",
+            border:"1px solid black" 
+        }}
+        >
+          <h4
+            style={{
+              margin: "0",
+              textDecoration: "none"
+            }}
+          >
+            {post.node.title}
+          </h4>
+          <p style={{margin:"0", color:"black"}}>
+            {post.node.description}
+          </p>
+        </div>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default PodcastList
